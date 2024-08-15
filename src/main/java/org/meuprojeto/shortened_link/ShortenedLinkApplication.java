2e894858c79aa9d82b6e19f4c03a35dca82b16c0
Jullianag <juh_g@hotmail.com>
2024-08-15 16:25:48 -0300

    iniciando repositorio

---
+++ src/main/java/org/meuprojeto/shortened_link/ShortenedLinkApplication.java
@@ -0,0 +1,13 @@
+package org.meuprojeto.shortened_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenedLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenedLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/meuprojeto/shortened_link/ShortenedLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meuprojeto.shortened_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenedLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
